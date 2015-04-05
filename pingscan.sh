7b7d029de83d45c960a76d4b59cba23589d7dc79
Jason Doelger <jdoelger@gmail.com>
2015-04-04 23:49:51 -0400

    pingscan sweeps 254 addresses

--- pingscan.sh
+++ pingscan.sh
@@ -3,3 +3,6 @@
 for ip in $(seq 1 125); do
 ping -c 1 192.168.2.$ip |grep "bytes from" |cut -d" " -f4 |cut -d":" -f1 &
 done
+for ip2 in $(seq 126 254); do
+ping -c 1 192.168.2.$ip2 |grep "bytes from" |cut -d" " -f4 |cut -d":" -f1 &
+done
